c18dec851dedaeae09ba687e2890a1ba29d54c3b
northtruths <zp1716120350@gmail.com>
2026-05-10 23:51:00 +0800

    feat: 添加默认格式的工厂函数声明

--- src/log_formatter.hpp
+++ src/log_formatter.hpp
@@ -3,12 +3,15 @@
 #include "log_message.hpp"
 #include <string>
 
-namespace log {
+namespace log
+{
 
-class Formatter {
-public:
-    virtual ~Formatter() = default;
-    virtual std::string format(const LogMsg& event) = 0;
-};
+    class Formatter
+    {
+    public:
+        virtual ~Formatter() = default;
+        virtual std::string format(const LogMsg &msg) = 0;
+    };
 
+    std::unique_ptr<Formatter> make_default_formatter();
 } // namespace log
