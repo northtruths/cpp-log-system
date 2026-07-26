b0b3517a2cd8758cb423dec9bfc686d270d3f589
northtruths <zp1716120350@gmail.com>
2026-07-26 12:34:46 +0800

    feat: 流式插入函数改为模版函数支持插入绝大部分数据类型

---
+++ include/log_stream.hpp
@@ -0,0 +1,30 @@
+#pragma once
+#include "log_level.hpp"
+#include <sstream>
+#include <string>
+
+namespace log
+{
+    class Logger;
+
+    class LogStream
+    {
+    public:
+        LogStream(Logger &owner, Level lv, const char *file, int line);
+        ~LogStream();
+
+        template <typename T>
+        LogStream &operator<<(const T &value)
+        {
+            oss_ << value;
+            return *this;
+        }
+
+    private:
+        Logger &owner_;
+        Level lv_;
+        const char *file_;
+        int line_;
+        std::ostringstream oss_;
+    };
+}
--- include/logger.hpp
+++ include/logger.hpp
@@ -5,18 +5,20 @@
 #include "log_formatter.hpp"
 #include "log_sink.hpp"
 #include "log_transmitter.hpp"
-#include "LogStream.hpp"
+#include "log_stream.hpp"
 #include <string>
 #include <memory>
 #include <atomic>
 #include <thread>
 #include <sstream>
+#include <signal.h>
 
 namespace log
 {
     class Logger
     {
         friend class LogStream;
+
     public:
         static Logger &instance()
         {
@@ -64,19 +66,19 @@ namespace log
             danger_level_ = lv;
         }
 
-        void operator()(const Level lv, const char *file, int line, const char *content)
+        void operator()(const Level lv, const char *file, int line, const std::string content)
         {
             if (lv < min_level_.load(std::memory_order_acquire))
                 return;
             LogMsg msg(lv, file, line, content);
-            // 格式化可以放进transmitter后台线程解决，可以减业务消耗，但这会稍微破坏框架设计
+            // 格式化可以放进transmitter后台线程解决，可以减业务消耗，但这会稍微破坏框架设计，可以后续优化
             formatter_->format(msg);
             transmitter_->send(msg.formatted_msg, sinks_, lv < Level::ERROR);
         }
 
         LogStream operator()(const Level lv, const char *file, int line)
         {
-            return LogStream(*this, lv,  file, line);
+            return LogStream(*this, lv, file, line);
         }
 
     private:
@@ -85,6 +87,7 @@ namespace log
         {
         }
 
+    private:
         std::atomic<Level> min_level_;
         std::unique_ptr<Formatter> formatter_;
         std::vector<std::unique_ptr<Sink>> sinks_;
@@ -92,22 +95,21 @@ namespace log
         Level danger_level_ = Level::ERROR;
     };
 
-    static Logger& logger = Logger::instance();
-    
+    static Logger &logger = Logger::instance();
 
-//字符串版 —— 直接传内容
-#define LOG_TRACE(content)   logger(log::Level::TRACE, __FILE__, __LINE__, content)
-#define LOG_DEBUG(content)   logger(log::Level::DEBUG, __FILE__, __LINE__, content)
-#define LOG_INFO(content)    logger(log::Level::INFO,  __FILE__, __LINE__, content)
-#define LOG_WARN(content)    logger(log::Level::WARN,  __FILE__, __LINE__, content)
-#define LOG_ERROR(content)   logger(log::Level::ERROR, __FILE__, __LINE__, content)
-#define LOG_FATAL(content)   logger(log::Level::FATAL, __FILE__, __LINE__, content)
+// 字符串版 —— 直接传内容
+#define LOG_TRACE(content) logger(log::Level::TRACE, __FILE__, __LINE__, content)
+#define LOG_DEBUG(content) logger(log::Level::DEBUG, __FILE__, __LINE__, content)
+#define LOG_INFO(content) logger(log::Level::INFO, __FILE__, __LINE__, content)
+#define LOG_WARN(content) logger(log::Level::WARN, __FILE__, __LINE__, content)
+#define LOG_ERROR(content) logger(log::Level::ERROR, __FILE__, __LINE__, content)
+#define LOG_FATAL(content) logger(log::Level::FATAL, __FILE__, __LINE__, content)
 
-//流式版 —— 不传 content，用 << 追加
-#define LOG_TRACE_STREAM()   logger(log::Level::TRACE, __FILE__, __LINE__)
-#define LOG_DEBUG_STREAM()   logger(log::Level::DEBUG, __FILE__, __LINE__)
-#define LOG_INFO_STREAM()    logger(log::Level::INFO,  __FILE__, __LINE__)
-#define LOG_WARN_STREAM()    logger(log::Level::WARN,  __FILE__, __LINE__)
-#define LOG_ERROR_STREAM()   logger(log::Level::ERROR, __FILE__, __LINE__)
-#define LOG_FATAL_STREAM()   logger(log::Level::FATAL, __FILE__, __LINE__)
+// 流式版 —— 不传 content，用 << 追加
+#define LOG_TRACE_STREAM() logger(log::Level::TRACE, __FILE__, __LINE__)
+#define LOG_DEBUG_STREAM() logger(log::Level::DEBUG, __FILE__, __LINE__)
+#define LOG_INFO_STREAM() logger(log::Level::INFO, __FILE__, __LINE__)
+#define LOG_WARN_STREAM() logger(log::Level::WARN, __FILE__, __LINE__)
+#define LOG_ERROR_STREAM() logger(log::Level::ERROR, __FILE__, __LINE__)
+#define LOG_FATAL_STREAM() logger(log::Level::FATAL, __FILE__, __LINE__)
 } // namespace log
